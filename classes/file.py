4a282798782d13c6ffc75f144b56f2ed8b004084
Eduardo Zimermam Pereira <eduardozimerman@live.com>
2021-11-03 21:05:36 -0300

    Adicionado o método para salvar um arquivo em um determinado path.

--- classes/file.py
+++ classes/file.py
@@ -1,3 +1,4 @@
+from _typeshed import NoneType
 import sys
 
 from utils.setup_logger import logger
@@ -34,3 +35,29 @@ class File():
         logger.info(f"Finalizada a leitura das sequências de peptídeos do arquivo: {seq_file}")
 
         return sequences
+
+    def save_file(self, path_to_save: str, content_to_save: NoneType) -> None:
+        """ Salva o conteúdo passado como parâmetro no path enviado como parâmetro"""
+
+        logger.info(f"Iniciando o processo de salvamento do conteúdo passado em: {path_to_save}")
+
+        time_init = time()
+
+        file = None
+
+        try:
+            file = open(path_to_save, 'w+')
+        except FileNotFoundError:
+            logger.error(f"Houve um erro ao tentar acessar o arquivo em: \"{path_to_save}\"")
+            sys.exit()
+
+        try:
+            file.write(content_to_save)
+        except FileExistsError:
+            logger.error(f"Houve um erro na tentativa de escrever no arquivo: \"{path_to_save}\"")
+            sys.exit()
+        
+        time_end = time()
+
+        logger.debug(f"Tempo gasto em segundos para escrever o arquivo {path_to_save}: {time_end - time_init} segundos")
+        logger.info(f"Finalizada a escrita no arquivo: {path_to_save}")
